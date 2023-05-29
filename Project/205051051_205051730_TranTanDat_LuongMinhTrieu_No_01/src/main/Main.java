6977d147037d9acac851603dad04aaef2ca96744
TranTanDat1162 <dattt11062002@gmail.com>
2023-05-29 18:54:55 +0700

    Tạo Module Login
    - Có username, password
    - Có chức năng mở rộng là Forgot Password
    - Đăng nhập thành công thì chuyển đến SalesFrame.

---
+++ Project/205051051_205051730_TranTanDat_LuongMinhTrieu_No_01/src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import frame.LoginFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LoginFrame loginFrame = new LoginFrame();
+        loginFrame.setVisible(false);
+	}
+
+}
