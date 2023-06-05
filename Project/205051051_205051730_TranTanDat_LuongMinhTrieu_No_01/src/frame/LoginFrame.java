86ab7702afa1864a3d071d09b22d1bbc3fbcc41e
TranTanDat1162 <dattt11062002@gmail.com>
2023-06-05 14:28:05 +0700

    Updated 2:27PM 5/6/2023
    Tạo thêm một file messages.properties

--- Project/205051051_205051730_TranTanDat_LuongMinhTrieu_No_01/src/frame/LoginFrame.java
+++ Project/205051051_205051730_TranTanDat_LuongMinhTrieu_No_01/src/frame/LoginFrame.java
@@ -3,48 +3,41 @@ package frame;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPasswordField;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-
-import model.User;
-
-import javax.swing.JTextField;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.imageio.ImageIO;
-import java.awt.Image;
-import java.io.IOException;
-import java.io.InputStream;
-
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
 import dao.UserDAO;
 import dao.UserDAOImpl;
+import model.User;
 
 public class LoginFrame extends JFrame {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1L;
     private JFrame frame;
     private JTextField usernameField;
     private JPasswordField passwordField;
     private UserDAO userDAO;
+    private ResourceBundle messages;
 
-    /**
-     * Launch the application.
-     */
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -58,12 +51,10 @@ public class LoginFrame extends JFrame {
         });
     }
 
-    /**
-     * Create the frame.
-     */
     public LoginFrame() {
-    	// Khởi tạo đối tượng UserDAOImpl
-    	userDAO =  new UserDAOImpl();
+        userDAO = new UserDAOImpl();
+        messages = ResourceBundle.getBundle("messages");
+
         frame = new JFrame();
         frame.setResizable(false);
         frame.setType(Type.UTILITY);
@@ -80,9 +71,8 @@ public class LoginFrame extends JFrame {
         lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 18));
         lblPassword.setBounds(376, 167, 90, 14);
         frame.getContentPane().add(lblPassword);
-        
-        JLabel lblForgotPassword;
-        lblForgotPassword = new JLabel("Forgot Password?");
+
+        JLabel lblForgotPassword = new JLabel("Forgot Password?");
         lblForgotPassword.setFont(new Font("Tahoma", Font.PLAIN, 18));
         lblForgotPassword.setBounds(520, 307, 141, 32);
         lblForgotPassword.setForeground(Color.BLUE);
@@ -113,13 +103,12 @@ public class LoginFrame extends JFrame {
 
                 if (user == null) {
                     // Người dùng không tồn tại
-                    JOptionPane.showMessageDialog(frame, "User does not exist");
+                    showMessage(messages.getString("userNotExist"), JOptionPane.ERROR_MESSAGE);
                 } else {
                     // Người dùng tồn tại
                     if (user.getPassword().equals(password)) {
                         // Đăng nhập thành công
-                        JOptionPane.showMessageDialog(frame, "Login Successful");
-                        // Mở module Bán Hàng
+                        showMessage(messages.getString("loginSuccessful"), JOptionPane.INFORMATION_MESSAGE);
                         try {
                             openSalesModule();
                         } catch (Exception e1) {
@@ -127,7 +116,7 @@ public class LoginFrame extends JFrame {
                         }
                     } else {
                         // Sai mật khẩu
-                        JOptionPane.showMessageDialog(frame, "Incorrect password");
+                        showMessage(messages.getString("incorrectPassword"), JOptionPane.ERROR_MESSAGE);
                     }
                 }
             }
@@ -143,105 +132,101 @@ public class LoginFrame extends JFrame {
         btnCancel.setFont(new Font("Tahoma", Font.BOLD, 18));
         btnCancel.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Xử lý hành động khi nhấn Cancel
-                // Ví dụ: Đóng ứng dụng
                 System.exit(0);
             }
         });
         btnCancel.setBounds(650, 243, 101, 37);
         frame.getContentPane().add(btnCancel);
-        
+
         JLabel lblNewLabel = new JLabel("Login Form");
         lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
         lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 24));
         lblNewLabel.setBounds(376, 11, 397, 56);
         frame.getContentPane().add(lblNewLabel);
 
-        // Thêm Logo UEF bên trái form
-		JLabel imageLabel = new JLabel();
-		try {
-		    InputStream inputStream = getClass().getResourceAsStream("/images/logouef.png");
-		    Image image = ImageIO.read(inputStream);
-		    Image scaledImage = image.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
-		    imageLabel.setIcon(new ImageIcon(scaledImage));
-		} catch (IOException e) {
-		    e.printStackTrace();
-		}
-		
-		imageLabel.setBounds(28, 32, 300, 307);
-		frame.getContentPane().add(imageLabel);
+        JLabel imageLabel = new JLabel();
+        try {
+            InputStream inputStream = getClass().getResourceAsStream("/images/logouef.png");
+            Image image = ImageIO.read(inputStream);
+            Image scaledImage = image.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
+            imageLabel.setIcon(new ImageIcon(scaledImage));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		// Thêm chức năng Quên mật khẩu
+        imageLabel.setBounds(28, 32, 300, 307);
+        frame.getContentPane().add(imageLabel);
 
-		lblForgotPassword.addMouseListener(new MouseAdapter() {
-		    @Override
-		    public void mouseClicked(MouseEvent e) {
-		        String username;
-		        User user;
+        lblForgotPassword.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                String username;
+                User user;
 
-		        do {
-		            username = JOptionPane.showInputDialog(frame, "Enter your username:");
-		            if (username != null) {
-		                user = userDAO.getPasswordByUsername(username);
-		                if (user == null) {
-		                    JOptionPane.showMessageDialog(frame, "Invalid username.");
-		                }
-		            } else {
-		                return;  // Người dùng nhấn Cancel
-		            }
-		        } while (user == null);
+                do {
+                    username = JOptionPane.showInputDialog(frame, messages.getString("enterUsername"));
+                    if (username != null) {
+                        user = userDAO.getPasswordByUsername(username);
+                        if (user == null) {
+                            showMessage(messages.getString("invalidUsername"), JOptionPane.ERROR_MESSAGE);
+                        }
+                    } else {
+                        return;
+                    }
+                } while (user == null);
 
-		        JPanel panel = new JPanel();
-		        panel.setLayout(new GridLayout(3, 2));
+                JPanel panel = new JPanel();
+                panel.setLayout(new GridLayout(3, 2));
 
-		        JPasswordField newPasswordField = new JPasswordField();
-		        JPasswordField confirmPasswordField = new JPasswordField();
+                JPasswordField newPasswordField = new JPasswordField();
+                JPasswordField confirmPasswordField = new JPasswordField();
 
-		        panel.add(new JLabel("New Password:"));
-		        panel.add(newPasswordField);
-		        panel.add(new JLabel("Confirm Password:"));
-		        panel.add(confirmPasswordField);
+                panel.add(new JLabel(messages.getString("newPassword")));
+                panel.add(newPasswordField);
+                panel.add(new JLabel(messages.getString("confirmPassword")));
+                panel.add(confirmPasswordField);
 
-		        int option;
-		        String newPassword;
-		        String confirmPassword;
-		        do {
-		            option = JOptionPane.showOptionDialog(frame, panel, "Change Password",
-		                    JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
+                int option;
+                String newPassword;
+                String confirmPassword;
+                do {
+                    option = JOptionPane.showOptionDialog(frame, panel, messages.getString("changePassword"),
+                            JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null, null, null);
 
-		            if (option == JOptionPane.OK_OPTION) {
-		                newPassword = String.valueOf(newPasswordField.getPassword());
-		                confirmPassword = String.valueOf(confirmPasswordField.getPassword());
+                    if (option == JOptionPane.OK_OPTION) {
+                        newPassword = String.valueOf(newPasswordField.getPassword());
+                        confirmPassword = String.valueOf(confirmPasswordField.getPassword());
 
-		                if (!newPassword.equals(confirmPassword)) {
-		                    JOptionPane.showMessageDialog(frame, "Passwords do not match.");
-		                }
-		            } else {
-		                return;  // Người dùng nhấn Cancel
-		            }
-		        } while (!newPassword.equals(confirmPassword));
+                        if (!newPassword.equals(confirmPassword)) {
+                            showMessage(messages.getString("passwordsDoNotMatch"), JOptionPane.ERROR_MESSAGE);
+                        }
+                    } else {
+                        return;
+                    }
+                } while (!newPassword.equals(confirmPassword));
 
-		        if (option == JOptionPane.OK_OPTION) {
-		            // Cập nhật mật khẩu mới vào đối tượng User
-		            user.setPassword(newPassword);
-		            // Thực hiện lưu mật khẩu mới vào cơ sở dữ liệu
-		            boolean updated = userDAO.updateUser(user);
-		            if (updated) {
-		                JOptionPane.showMessageDialog(frame, "Password changed successfully.");
-		            } else {
-		                JOptionPane.showMessageDialog(frame, "Failed to change password.");
-		            }
-		        }
-		    }
-		});
+                if (option == JOptionPane.OK_OPTION) {
+                    user.setPassword(newPassword);
+                    boolean updated = userDAO.updateUser(user);
+                    if (updated) {
+                        showMessage(messages.getString("passwordChanged"), JOptionPane.INFORMATION_MESSAGE);
+                    } else {
+                        showMessage(messages.getString("failedToChangePassword"), JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            }
+        });
 
         frame.setVisible(true);
-   		}
-
-    private void openSalesModule() {
-    	frame.dispose();
-        SalesFrame salesFrame = new SalesFrame(); 
-        salesFrame.setVisible(true); 
     }
 
+    private void showMessage(String message, int messageType) {
+        JOptionPane.showMessageDialog(frame, message, "Message", messageType);
+    }
+
+    private void openSalesModule() {
+        frame.dispose();
+        SalesFrame salesFrame = new SalesFrame();
+        salesFrame.setVisible(true);
+    }
 }
